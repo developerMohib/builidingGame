6f72d73077f01970286fe8ae806dd6c4f0d21436
Mohib <mohibullahmohim2020@gmail.com>
2024-02-14 15:05:35 +0600

    js version comming soon

---
+++ alpha-clash.js
@@ -0,0 +1,10 @@
+function playNow(){
+    // step 1 :- click play Now button and hid the Home section
+    let homeSection = document.getElementById('homeScreen');
+    homeSection.classList.add('hidden');
+    // console.log(homeSection);
+
+    // step 2 :- while play Now button clicked visible the playGround section
+    let playGround = document.getElementById('playGround');
+    playGround.classList.remove('hidden');
+}
