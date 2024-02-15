864dc4ff42ec5acf1ecdbcd52d8713aceb2cf1d5
Mohib <mohibullahmohim2020@gmail.com>
2024-02-15 22:14:39 +0600

    added something

--- alpha-clash-utility.js
+++ alpha-clash-utility.js
@@ -12,12 +12,13 @@ function showlEmentById(elementId){
 /* ------ Keyboard Selection ------ */
 
 function getRandomAlphabet(){
+    
     // generate array
     let myAlphabetString = 'a b c d e f g h i j k l m n o p q r s t u v w x y z';
     let myAlphabet = myAlphabetString.split(' ');
     console.log(myAlphabet);
-    // get a random number 
 
+    // get a random number 
     let myRandom = Math.random() * 25 ;
     let randomIndex = Math.round(myRandom);
 
